265a2eb5187761edb37af53535e64d3c8954412d
Yuriy Zemskov <zemskyura@gmail.com>
2022-08-09 12:13:01 +0300

    Валидаторы в модели Pydantic для Item.

--- src/fastapiexample/models.py
+++ src/fastapiexample/models.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 from typing import Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
     name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
+    description: Union[str, None] = Field(default=None, title="Description of the item.", max_length=128)
+    price: float = Field(gt=0, description="Price must be greater than zero.")
+    tax: Union[float, None] = Field(ge=0)  # can be either null or non-negative
