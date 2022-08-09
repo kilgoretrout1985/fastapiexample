b5a3e72bbecdf26b374b35f540fa236cdb69dfae
Yuriy Zemskov <zemskyura@gmail.com>
2022-08-09 12:46:43 +0300

    Example schema for Item model (used in api docs).

--- src/fastapiexample/models.py
+++ src/fastapiexample/models.py
@@ -10,3 +10,13 @@ class Item(BaseModel):
     description: Union[str, None] = Field(default=None, title="Description of the item.", max_length=128)
     price: float = Field(gt=0, description="Price must be greater than zero.")
     tax: Union[float, None] = Field(ge=0)  # can be either null or non-negative
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Nail",
+                "description": "Also buy a hummer for your nails.",
+                "price": 0.8,
+                "tax": 0.01,
+            }
+        }
